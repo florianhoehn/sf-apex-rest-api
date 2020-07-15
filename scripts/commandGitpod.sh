9e681b1ba6e659d0fc6895849bfd6824ac92105a
Florian Hoehn <fhoehn@mavens.com>
2020-07-15 12:22:57 +0000

    Add DX initial commands to set up workspace

--- scripts/commandGitpod.sh
+++ scripts/commandGitpod.sh
@@ -1,1 +1,4 @@
-- command: echo "$DEVHUBKEY" > config/devhub-server.key
+echo "$DEVHUBKEY" > config/devhub-server.key
+sed -i "s/||n||/\n/gi" config/devhub-server.key
+npm install sfdx-cli --global
+sfdx force:auth:jwt:grant --clientid $DEVHUBCONSUMERKEY --jwtkeyfile config/devhub-server.key --username $DEVHUBUSERNAME --setdefaultdevhubusername -a DEVHUB
--- scripts/initGitpod.sh
+++
@@ -1,2 +0,0 @@
-npm install sfdx-cli --global
-sfdx force:auth:jwt:grant --clientid $DEVHUBCONSUMERKEY --jwtkeyfile config/devhub-server.key --username $DEVHUBUSERNAME --setdefaultdevhubusername -a DEVHUB
