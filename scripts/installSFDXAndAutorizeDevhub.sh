159c1b1202e4efea4fc1da0c212998320e91de9b
Florian Hoehn <fhoehn@mavens.com>
2021-03-12 20:58:43 +0000

    Fix SFDX install with npm

--- scripts/installSFDXAndAutorizeDevhub.sh
+++ scripts/installSFDXAndAutorizeDevhub.sh
@@ -2,5 +2,5 @@
 
 echo "$DEVHUBKEY" > config/devhub-server.key
 sed -i "s/||n||/\n/gi" config/devhub-server.key
-yarn && yarn global add sfdx-cli
+npm install --global sfdx-cli
 bash scripts/dx/authorizeDevhub.sh
